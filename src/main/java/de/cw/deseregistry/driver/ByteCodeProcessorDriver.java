38acfcc9deb7052aba77da1b9c478a7d55b1185c
supersache <u2lrich@posteo.de>
2019-10-18 13:55:52 +0200

    First steps with byte code processor.

--- src/main/java/de/cw/deseregistry/driver/ByteCodeProcessorDriver.java
+++ src/main/java/de/cw/deseregistry/driver/ByteCodeProcessorDriver.java
@@ -1,8 +1,13 @@
 package de.cw.deseregistry.driver;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashMap;
@@ -13,6 +18,7 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipException;
 import java.util.zip.ZipFile;
 
+import de.cw.deseregistry.errorhandling.FatalException;
 import de.cw.deseregistry.main.IDriver;
 import de.cw.deseregistry.main.Listener;
 import de.cw.deseregistry.main.Listener_Action;
@@ -27,6 +33,50 @@ public class ByteCodeProcessorDriver implements IDriver {
 	private List<Listener> addExtendsListener = new ArrayList<Listener>();
 	private List<Listener> addMethodListener = new ArrayList<Listener>();
 	private List<Listener> classLoadErrorListener = new ArrayList<Listener>();
+	private PrintWriter visitedFile = null;
+	
+	public ByteCodeProcessorDriver (File visitedFile)
+	{
+		prepareVisitedFile (visitedFile);
+		jarfilesToProcess = new ArrayList<File> ();
+	}
+	
+	/**
+	 * Kümmert sich um die Datei, die schon analysierte Klassen speichert
+	 * @param visitedFile Parameter aus der Config der die Datei benennt
+	 */
+	private void prepareVisitedFile (File visitedFile)
+	{
+		try {
+			if (visitedFile.exists ()) {
+				/**
+				 *  Wenn die Datei existiert müssen zuerst die schon besuchten Klassen
+				 *  eingelesen werden damit man nachher da weitermachen kann
+				 *  wo man das letzte Mal aufgehört hat
+				 */
+				BufferedReader br = new BufferedReader (new InputStreamReader (new FileInputStream (visitedFile)));
+				String line = null;
+				while (null != (line = br.readLine ())) {
+					this.visitedClasses.add (line);
+				}
+				// wenn wir hier nicht close machen funktioniert die 
+				// darauf folgende Anweisung nicht.
+				br.close();
+				// damit wir eine neue Datei schreiben können
+				visitedFile.delete ();
+			}
+			this.visitedFile = new PrintWriter (new FileOutputStream (visitedFile));
+		}
+		catch (FileNotFoundException e) {
+			e.printStackTrace(System.err);
+			throw new FatalException ("Cannot create Visited File.");
+		}
+		catch (IOException e) {
+			e.printStackTrace (System.err);
+			throw new FatalException ("Stress with visited file.");
+		}			
+	}	
+	
 	
 	/**
 	 * 
@@ -56,9 +106,12 @@ public class ByteCodeProcessorDriver implements IDriver {
 	public void go() throws Exception
 	{
 		fillClassToJarMap ();
+		String s = this.classToProcess.get(0);
+		byte [] bb = this.getByteCode(s);
 	}
 	
 	private byte [] getByteCode (String className)
+			throws IOException
 	{
 		final String CMD = "/bin/sh -c $@|sh . echo cd /tmp/ && jar -xvf %s %s";
 		File jarFile = this.classToJarfileMap.get (className);
@@ -72,16 +125,19 @@ public class ByteCodeProcessorDriver implements IDriver {
 			else {
 				File f = new File ("/tmp/" + className + ".class");
 				long len = f.length ();
-				byte _bb_ = new byte [len];
+				if (len > new Long(Integer.MAX_VALUE)) {
+					throw new IOException ("Class file too big.");
+				}
+				byte[] _bb_ = new byte [(int) len];
 				FileInputStream fis = new FileInputStream (f);
-				
+				fis.read(_bb_);
+				return _bb_;
 			}
 		} catch (IOException | InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		
-		
+			return null;
+		}		
 	}
 	
 	/**
